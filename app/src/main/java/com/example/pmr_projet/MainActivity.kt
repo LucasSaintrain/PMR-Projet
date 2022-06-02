73ea25f50dc5fffd0533d40fa37bf93ef4d6aa83
Lucas <lucasosaintrain@gmail.com>
2022-06-02 08:50:35 +0200

    Added second button to the simple interface

--- app/src/main/java/com/example/pmr_projet/MainActivity.kt
+++ app/src/main/java/com/example/pmr_projet/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.pmr_projet
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val voiceButton: Button = findViewById(R.id.buttonVoice)
-
         voiceButton.setOnClickListener{
             val intent = Intent(this, VoskActivity::class.java)
             startActivity(intent)
         }
 
+        val sceneviewButton: Button = findViewById(R.id.buttonSceneview)
+        sceneviewButton.setOnClickListener{
+            val intent = Intent(this, ArSceneActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
